72a0f3241f888a25a599f12aeac97257fbb227e2
Aparna Komarla <aparna.komarla@gmail.com>
2024-02-12 01:27:10 -0800

    Testing new function to generate implied offenses

--- resentencing_data_initiative/code/play.py
+++ resentencing_data_initiative/code/play.py
@@ -9,86 +9,99 @@ import pandas as pd
 import copy
 from helpers import *
 
-criteria = pd.read_excel('C:/Users/apkom/Downloads/sorting_criteria.xlsx', dtype = {'Offenses': str})
+criteria = pd.read_excel('C:/Users/akomarla/Downloads/sorting_criteria.xlsx', dtype = {'Offenses': str})
 current_commits = pd.read_excel('C:/Users/apkom/Downloads/currentcommitments.xlsx')
 demographics = pd.read_excel('C:/Users/apkom/Downloads/demographics.xlsx')
 
-impl = {'all': ['/att', '(664)', '2nd', "(ss)"], '459': ['/att', '(664)']}
-excpt = [key for key in impl.keys() if key != 'all']
+impl_rel = {'all': ['/att', '(664)', '2nd', "(ss)"], '459': ['/att', '(664)']}
 offenses = clean_offense_blk(criteria[criteria['Table'] == 'Table F']['Offenses'].to_list())
 
-# Remove exceptions
-offenses_woe = copy.deepcopy(offenses)
-for e in impl.keys():
-    if (e != 'all') and (e in offenses):
-        offenses_woe.remove(e)
 
-def gen_impl_val(use, perm, sep = '', fix = None, pos = None):
-    if fix and pos == 0:
-        res = [sep.join(v) for v in list(product(use, fix, repeat=1))]
-    elif fix and pos == 1:
-        res = [sep.join(v) for v in list(product(fix, use, repeat=1))]
-    else:
-        res = []
+def gen_impl_off(offenses, 
+                 impl_rel, 
+                 perm = 3,
+                 sep = '', 
+                 fix_pos = {"2nd": 0, "(ss)": 0}, 
+                 placeholder = {"ss": ['a', 'b', 'c']}):
+    
+    # Remove exceptions from the list of offenses
+    offenses_woe = copy.deepcopy(offenses)
+    for e in impl_rel.keys():
+        if (e != 'all') and (e in offenses):
+            offenses_woe.remove(e)
+    
+    # Initialize list of implied offenses
+    impl_off = []
+    for rel in impl_rel.keys():
+        # Generate list of implied values
+        impl_val = gen_impl_val(impl = impl_rel[rel],  
+                                sep = sep, 
+                                perm = perm,
+                                fix_pos = fix_pos, 
+                                placeholder = placeholder)
         
-    for p in range(2, perm+1):
-        for v in list(permutations(res, p)):
-            res.append(sep.join(v))
-    res.extend(use)
-    return res
+        # If offense is NOT an exception
+        if rel == 'all':
+            # Adding implications to all offenses that do NOT have an exception
+            for owe in offenses_woe:
+                for iv in impl_val:
+                    impl_off.append(owe+iv)
         
-sep = ''
-perm = 2
-
-impl_offenses = {}
-for val in impl.keys():
-    if val == 'all':
-        # Generate implied offenses
-        add = [sep.join(t) for t in list(permutations(impl[val], perm))]
-        for a in add:
-            impl_offenses = list(set.union(set(impl_offenses), set([off+sep+a for off in offenses_woe])))
-    # If val is an exception and called out separately
-    else:
-        # Generate implied offenses
-        add = [sep.join(t) for t in list(permutations(impl[val], perm))]
-        for a in add:
-            impl_offenses = list(set.union(set(impl_offenses), set([off+sep+a for off in offenses if val in off])))
-
-
-
-
-res = [('')]
-attach = impl['all'][:]
-for i in range(1, len(attach)+1):
-    res.extend(list(permutations(attach, i)))
-
-fix_pos = {"2nd": 0,
-           "(ss)": 0}
-
-# Only select permutations that have at least one of the specified fixed values at the given position. Not ALL of the fixed values will be at the specified position.
-sel = []
-for r in res:
-    for f in fix_pos.keys():
-        try: 
-            if (f in r) and (r[fix_pos[f]] == f) and (r not in sel):
-                sel.append(r)
-            elif (f not in r) and (r not in sel):
-                sel.append(r)
-        except:
-            print(r, 'error')
-
-# Combine the tuples into a single string 
-sep = ''
-sel = [sep.join(s) for s in sel]
-
-# Replace placeholder values with actual ones 
-placeholder = {"ss": ['a', 'b', 'c']}
-
-for p in placeholder.keys():
-    new = []
-    for repl in placeholder[p]:
-        for s in sel:
-            new.append(s.replace(p, repl))
+        # If offense is an exception
+        elif rel != 'all':
+            # Adding implications to the exception offense (always called out individually)
+            for iv in impl_val:
+                impl_off.append(rel+iv)
+    
+    return impl_off
+            
+            
+    
+def gen_impl_val(impl,  
+                 sep = '', 
+                 perm = 3, 
+                 fix_pos = {"2nd": 0, "(ss)": 0}, 
+                 placeholder = {"ss": ['a', 'b', 'c']}):
+    
+    # Initialize list of permutations of implied values
+    res = []
+    for i in range(1, perm+1):
+        # Add permutations for each count
+        res.extend(list(permutations(impl, i)))
+    
+    # Create a new set of resultant implied values to perform manipulations if requested
+    sel = copy.deepcopy(res)
+    
+    # If some values should have fixed positions
+    if fix_pos:
+        # Remove implied values that do not have ANY of the fixed values in the speciied positions
+        for r in res:
+            # Only select permutations that have at least one of the specified fixed values at the given position. 
+            if (any(f in r for f in fix_pos.keys())):
+                # Not ALL of the fixed values will be at the specified position. Check if ANY of the fixed values are at the specified position.
+                if (any(r[fix_pos[f]] == f for f in fix_pos.keys())):
+                    pass
+                # Remove permutations that do not have any of the fixed values at the specified positions
+                else:
+                    sel.remove(r)
+    
+    # Combine the tuples into a single string (necessary step since no manipulations can be made on tuple values)
+    sel = [sep.join(s) for s in sel]
+    
+    # If implied value is a placeholder for other values
+    if placeholder: 
+        # Replace placeholder values with actual ones 
+        new = []
+        for p in placeholder.keys():
+            # Perform replacement for each value
+            for repl in placeholder[p]:
+                for s in sel:
+                    new.append(s.replace(p, repl))
+        # If placeholders were requested
+        return list(set(new))
+    
+    # If there are no placeholders return the original list of strings
+    return list(set(sel))
             
 
         
