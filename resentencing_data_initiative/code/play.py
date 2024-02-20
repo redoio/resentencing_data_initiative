7ce6cb57507124b6b16ed1db3f7cad5b4603be2e
Aparna Komarla <aparna.komarla@gmail.com>
2024-02-20 01:23:39 -0800

    Checking output of new and old impl_off generators

--- resentencing_data_initiative/code/play.py
+++ resentencing_data_initiative/code/play.py
@@ -10,8 +10,8 @@ import copy
 from helpers import *
 
 criteria = pd.read_excel('C:/Users/akomarla/Downloads/sorting_criteria.xlsx', dtype = {'Offenses': str})
-current_commits = pd.read_excel('C:/Users/apkom/Downloads/currentcommitments.xlsx')
-demographics = pd.read_excel('C:/Users/apkom/Downloads/demographics.xlsx')
+#current_commits = pd.read_excel('C:/Users/apkom/Downloads/currentcommitments.xlsx')
+#demographics = pd.read_excel('C:/Users/apkom/Downloads/demographics.xlsx')
 
 impl_rel = {'all': ['/att', '(664)', '2nd', "(ss)"], '459': ['/att', '(664)']}
 offenses = clean_offense_blk(criteria[criteria['Table'] == 'Table F']['Offenses'].to_list())
@@ -22,7 +22,8 @@ def gen_impl_off(offenses,
                  perm = 3,
                  sep = '', 
                  fix_pos = {"2nd": 0, "(ss)": 0}, 
-                 placeholder = {"ss": ['a', 'b', 'c']}):
+                 placeholder = {"ss": ['a', 'b', 'c']}, 
+                 how = 'inclusive'):
     
     # Remove exceptions from the list of offenses
     offenses_woe = copy.deepcopy(offenses)
@@ -30,8 +31,13 @@ def gen_impl_off(offenses,
         if (e != 'all') and (e in offenses):
             offenses_woe.remove(e)
     
-    # Initialize list of implied offenses
-    impl_off = []
+    if how == 'inclusive':
+        # Initialize list of implied offenses - add the baseline offenses
+        impl_off = offenses[:]
+    else:
+        impl_off = []
+        
+    # Add the implied offenses
     for rel in impl_rel.keys():
         # Generate list of implied values
         impl_val = gen_impl_val(impl = impl_rel[rel],  
@@ -48,12 +54,12 @@ def gen_impl_off(offenses,
                     impl_off.append(owe+iv)
         
         # If offense is an exception
-        elif rel != 'all':
+        elif (rel != 'all') and (rel in offenses):
             # Adding implications to the exception offense (always called out individually)
             for iv in impl_val:
                 impl_off.append(rel+iv)
     
-    return impl_off
+    return list(set(impl_off))
             
             
     
@@ -76,7 +82,7 @@ def gen_impl_val(impl,
     if fix_pos:
         # Remove implied values that do not have ANY of the fixed values in the speciied positions
         for r in res:
-            # Only select permutations that have at least one of the specified fixed values at the given position. 
+            # Only select permutations that have at least one of the specified fixed values at the given position
             if (any(f in r for f in fix_pos.keys())):
                 # Not ALL of the fixed values will be at the specified position. Check if ANY of the fixed values are at the specified position.
                 if (any(r[fix_pos[f]] == f for f in fix_pos.keys())):
